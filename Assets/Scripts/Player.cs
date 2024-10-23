92f7d6df9bb1e1505f0ef6d82db5d2aa8854a1bc
acise314 <puyuhuan1@gmail.com>
2024-10-22 21:24:00 -0400

    finished animations, bullets, movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,83 +1,108 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
 
-public class Player : MonoBehaviour
-{
-
-    public float moveForce = 5f;
-    public float jumpForce = 11f;
-    public float movementX;
-    public float movementY;
-
-    public Rigidbody2D myBody;
-    private SpriteRenderer sr;
-    private Animator anim;
-    private string RUN_ANIMATION = "Move";
-    private bool facingRight = true;
-
-
-    void Awake()
-    {
-        this.myBody = GetComponent<Rigidbody2D>();
-        this.anim = GetComponent<Animator>();
-        this.sr = GetComponent<SpriteRenderer>();
-
-    }
-    public void AnimatePlayer()
+    public class Player : MonoBehaviour
     {
 
-        if (this.movementX != 0 || this.movementY != 0)
+        public float moveForce = 5f;
+        public float jumpForce = 11f;
+        public float movementX;
+        public float movementY;
+
+        public Rigidbody2D myBody;
+        private SpriteRenderer sr;
+        private Animator anim;
+        private string RUN_ANIMATION = "Move";
+        private bool facingRight = true;
+
+    public GameObject bulletPrefab;
+    public int numBullets = 1;
+    public float bulletSpeed = 10f;
+    public Transform firePoint;
+    public Vector2 lookDirection;
+    float lookAngle;
+
+
+        void Awake()
         {
-            anim.SetBool(RUN_ANIMATION, true);
+            this.myBody = GetComponent<Rigidbody2D>();
+            this.anim = GetComponent<Animator>();
+            this.sr = GetComponent<SpriteRenderer>();
+            this.anim.speed = 0;
+
         }
-        else
+        public void AnimatePlayer()
         {
-            anim.SetBool(RUN_ANIMATION, false);
+
+            if (this.movementX != 0)
+            {
+                anim.SetBool(RUN_ANIMATION, true);
+
+        anim.speed = 0.75f;
+            }
+            else
+            {
+                anim.SetBool(RUN_ANIMATION, false);
+        anim.speed = 0;
+
+            }
         }
-    }
 
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        PlayerMoveKeyboard();
-    }
-    void PlayerMoveKeyboard()
-    {
-        this.movementX = Input.GetAxis("Horizontal");
-        Debug.Log("move X value is: " + this.movementX);
-        if (movementX < 0){
-                FlipCharacter(true);
+        void Start()
+        {
 
         }
-        else{
-                FlipCharacter(false);
+
+        // Update is called once per frame
+        void Update()
+        {
+            PlayerMoveKeyboard();
+            BulletShoot();
 
         }
-        this.movementY = Input.GetAxis("Vertical");
-        Debug.Log("move Y value is: " + this.movementY);
+        void BulletShoot(){
+        lookDirection = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        lookAngle = Mathf.Atan2(lookDirection.y, lookDirection.x)*Mathf.Rad2Deg;
+                firePoint.rotation = Quaternion.Euler(0, 0, lookAngle);
+             if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+            Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+            rb.velocity = firePoint.right * bulletSpeed;
+        }
+
+        }
+        void PlayerMoveKeyboard()
+        {
+            this.movementX = Input.GetAxis("Horizontal");
+            Debug.Log("move X value is: " + this.movementX);
+            if (movementX < 0){
+                    FlipCharacter(false);
+
+            }
+            else if (movementX > 0){
+                    FlipCharacter(true);
+            }
+            this.movementY = Input.GetAxis("Vertical");
+            Debug.Log("move Y value is: " + this.movementY);
 
 
 
 
-        AnimatePlayer();
+            AnimatePlayer();
 
 
-        Vector2 movement = new Vector2(this.movementX * this.moveForce, this.movementY * this.moveForce);
-        this.myBody.velocity = movement;
+            Vector2 movement = new Vector2(this.movementX * this.moveForce, this.movementY * this.moveForce);
+            this.myBody.velocity = movement;
+
+        }
+            void FlipCharacter(bool faceRight)
+        {
+            facingRight = faceRight;
+            Vector3 localScale = transform.localScale;
+            localScale.x = faceRight ? 1 : -1;
+            transform.localScale = localScale;
+        }
 
     }
-        void FlipCharacter(bool faceRight)
-    {
-        facingRight = faceRight;
-        Vector3 localScale = transform.localScale;
-        localScale.x = faceRight ? 1 : -1;
-        transform.localScale = localScale;
-    }
-
-}
