6d4bec0f6737255eb9803f78247e4cd1046cb7ed
Domino-wolf <regulus.zeng25@ycdsbk12.ca>
2024-10-24 18:15:01 -0400

    Edited the player and character scripts to accommodate for  the powerups that I will soon add

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -1,33 +1,23 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Character : MonoBehaviour
 {
     private Player mainCharacter;
 
-    // Assign these via the Unity Editor or find them in Start()
     public Rigidbody2D body;
     public Animator animator;
     public SpriteRenderer spriteRenderer;
     public GameObject bulletPrefab;
     public Transform firePoint;
     public GameObject hearts;
-    public GameObject pointer;
-    public Transform playerTransform;
 
-    // Start is called before the first frame update
     void Start()
     {
-        mainCharacter = new Player(this, playerTransform, body, animator, spriteRenderer, bulletPrefab, firePoint, hearts, pointer);
+        mainCharacter = new Player(this, body, animator, spriteRenderer, bulletPrefab, firePoint, hearts);
     }
 
-    // Update is called once per frame
     void Update()
     {
         mainCharacter.FrameChange();
     }
-    
-
-  
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,7 +1,3 @@
-// smt from b1, b2
-
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Player
@@ -11,33 +7,18 @@ public class Player
     private Animator _anim;
     private GameObject _bulletPrefab;
     private GameObject _hearts;
-    private GameObject _pointer;
     private Transform _firePoint;
-
     private int _health = 10;
-    private float _moveforce = 5f; // same as speed
+    private float _moveForce = 5f;
     private float _damage = 5f;
-    private int _numBullets = 1;
-    private float _jumpForce = 11f;
-    private float _rotationSpeed = 100f;
-    private KeyCode _spinKeyClockwise = KeyCode.J;
-    private KeyCode _shootKey = KeyCode.K;
-    private KeyCode _spinKeyCounterClockwise = KeyCode.L;
-
-    private float _movementX;
-    private float _movementY;
     private float _bulletSpeed = 10f;
-    private Vector2 _lookDirection;
-    private float _lookAngle;
 
     private const string RUN_ANIMATION = "Move";
     private bool _facingRight = true;
-    private float _heartLocation = 4.5f;
-    private Transform _playerTransform;
 
     private MonoBehaviour _character;
 
-    public Player(MonoBehaviour character, Transform playerTransform, Rigidbody2D body, Animator animator, SpriteRenderer spriteRenderer, GameObject bullet, Transform firePoint, GameObject heart, GameObject pointer)
+    public Player(MonoBehaviour character, Rigidbody2D body, Animator animator, SpriteRenderer spriteRenderer, GameObject bullet, Transform firePoint, GameObject heart)
     {
         this._myBody = body;
         this._anim = animator;
@@ -45,48 +26,59 @@ public class Player
         this._bulletPrefab = bullet;
         this._firePoint = firePoint;
         this._hearts = heart;
-        this._pointer = pointer;
-        this._playerTransform = playerTransform;
-        this._anim.speed = 0; 
         this._character = character;
-        showHearts();
-
+        ShowHearts();
     }
 
     public void FrameChange()
     {
-        playerMoveKeyboard();
-        bulletShoot();
-        spin();
-
+        PlayerMoveKeyboard();
+        BulletShoot();
     }
-    private void showHearts()
+
+    public void AddHealth(int amount)
+    {
+        _health += amount;
+        ShowHearts();
+    }
+
+    public void AddDamage(float amount)
+    {
+        _damage += amount;
+    }
+
+    private void ShowHearts()
     {
         for (int i = 0; i < this._health; i++)
         {
-            Vector2 heartPosition = new Vector2(-this._heartLocation + (0.3f * i), this._heartLocation);
-            GameObject heart = UnityEngine.Object.Instantiate(this._hearts, heartPosition, Quaternion.identity, this._playerTransform);
-          //  heart.transform.SetParent(this._playerTransform, false);
+            Vector2 heartPosition = new Vector2(-4.5f + (0.3f * i), 4.5f);
+            GameObject heart = UnityEngine.Object.Instantiate(this._hearts, heartPosition, Quaternion.identity);
         }
     }
 
-    private void animatePlayer()
+    private void PlayerMoveKeyboard()
     {
-        if (this._movementX != 0 || this._movementY != 0)
+        float movementX = Input.GetAxis("Horizontal");
+        float movementY = Input.GetAxis("Vertical");
+
+        Vector2 movement = new Vector2(movementX * this._moveForce, movementY * this._moveForce);
+        this._myBody.velocity = movement;
+
+        if (movementX < 0)
         {
-            this._anim.SetBool(RUN_ANIMATION, true);
-            this._anim.speed = 0.75f;
+            FlipCharacter(false);
         }
-        else
+        else if (movementX > 0)
         {
-            this._anim.SetBool(RUN_ANIMATION, false);
-            this._anim.speed = 0;
+            FlipCharacter(true);
         }
+
+        _anim.SetBool(RUN_ANIMATION, movementX != 0 || movementY != 0);
     }
 
-    private void bulletShoot()
+    private void BulletShoot()
     {
-        if (Input.GetKeyDown(this._shootKey))
+        if (Input.GetKeyDown(KeyCode.K))
         {
             GameObject bullet = UnityEngine.Object.Instantiate(this._bulletPrefab, this._firePoint.position, this._firePoint.rotation);
             Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
@@ -95,54 +87,12 @@ public class Player
         }
     }
 
-    private void playerMoveKeyboard()
+    private void FlipCharacter(bool faceRight)
     {
-        spin();
-        this._movementX = Input.GetAxis("Horizontal");
-        Debug.Log("move X value is: " + this._movementX);
-        if (this._movementX < 0)
+        if (_facingRight != faceRight)
         {
-            flipCharacter(false);
+            _sr.flipX = !_sr.flipX;
         }
-        else if (this._movementX > 0)
-        {
-            flipCharacter(true);
-        }
-        this._movementY = Input.GetAxis("Vertical");
-        Debug.Log("move Y value is: " + this._movementY);
-
-        animatePlayer();
-
-        Vector2 movement = new Vector2(this._movementX * this._moveforce, this._movementY * this._moveforce);
-        this._myBody.velocity = movement;
-    }
-
-    private void flipCharacter(bool faceRight)
-    {
-        if (this._facingRight != faceRight)
-        {
-            this._sr.flipX = !this._sr.flipX;
-        }
-        this._facingRight = faceRight;
-    }
-
-    private void spin()
-    {
-        if (Input.GetKey(this._spinKeyClockwise))
-        {
-            this._firePoint.Rotate(0, 0, this._rotationSpeed * Time.deltaTime);
-        }
-        if (Input.GetKey(this._spinKeyCounterClockwise))
-        {
-            this._firePoint.Rotate(0, 0, -1 * this._rotationSpeed * Time.deltaTime);
-        }
-        updatePointer();
-    }
-
-    private void updatePointer()
-    {
-        this._pointer.transform.position = this._firePoint.position;
-        float angle = this._firePoint.eulerAngles.z - 45.0f;
-        this._pointer.transform.rotation = Quaternion.Euler(0, 0, angle);
+        _facingRight = faceRight;
     }
 }
